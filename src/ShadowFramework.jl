caaa4f0daee8b97e7f4cdc6224544e0f48852c69
Octogonapus <me@irrational.engineering>
2025-03-20 11:01:13 -0400

    Cleanup (#31)
    * Fix warning rendering
    
    * Remove unused internal functions
    
    * patch bump

--- src/ShadowFramework.jl
+++ src/ShadowFramework.jl
@@ -259,11 +259,12 @@ end
 
 Blocks until the next time the shadow document is synchronized with the broker.
 
-    !!! warning "Warning: Race Condition"
-        If you are using this function to publish a message and then wait for the following synchronization, you
-        must use the `do` form of [`wait_until_synced`](@ref) instead which accepts a lambda as the first argument.
-        Otherwise, your publish will race the synchronization and there is a chance the synchronization will finish before
-        you begin waiting for it (so you miss the edge and your program hangs).
+!!! warning "Warning: Race Condition"
+
+    If you are using this function to publish a message and then wait for the following synchronization, you
+    must use the `do` form of [`wait_until_synced`](@ref) instead which accepts a lambda as the first argument.
+    Otherwise, your publish will race the synchronization and there is a chance the synchronization will finish before
+    you begin waiting for it (so you miss the edge and your program hangs).
 """
 wait_until_synced(sf::ShadowFramework) = wait_until_synced(() -> nothing, sf)
 
@@ -514,7 +515,5 @@ _version_allows_update(doc::AbstractDict, version::Int) = version >= _version(do
 _version_allows_update(doc::AbstractDict, version::Nothing) = false
 
 _version(doc::AbstractDict) = doc["version"]
-_version(doc) = doc.version
 
 _set_version!(doc::AbstractDict, version::Int) = doc["version"] = version
-_set_version!(doc, version::Int) = doc.version = version
