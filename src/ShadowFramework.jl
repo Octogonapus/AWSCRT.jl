2d2b5363b622c8702fb248f9a8b2f4cda8ef6a39
Octogonapus <me@irrational.engineering>
2025-03-20 10:30:10 -0400

    Fix warning rendering

--- src/ShadowFramework.jl
+++ src/ShadowFramework.jl
@@ -259,11 +259,12 @@ end
 
 Blocks until the next time the shadow document is synchronized with the broker.
 
-    !!! warning "Warning: Race Condition"
-        If you are using this function to publish a message and then wait for the following synchronization, you
-        must use the `do` form of [`wait_until_synced`](@ref) instead which accepts a lambda as the first argument.
-        Otherwise, your publish will race the synchronization and there is a chance the synchronization will finish before
-        you begin waiting for it (so you miss the edge and your program hangs).
+!!! warning "Warning: Race Condition"
+
+    If you are using this function to publish a message and then wait for the following synchronization, you
+    must use the `do` form of [`wait_until_synced`](@ref) instead which accepts a lambda as the first argument.
+    Otherwise, your publish will race the synchronization and there is a chance the synchronization will finish before
+    you begin waiting for it (so you miss the edge and your program hangs).
 """
 wait_until_synced(sf::ShadowFramework) = wait_until_synced(() -> nothing, sf)
 
